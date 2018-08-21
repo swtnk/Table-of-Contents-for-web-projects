995ccbe575ca6d8eaec3afa7c595a835932517fc
Swetank Subham <swetanksubham.r@gmail.com>
2018-08-22 00:28:16 +0530

    1.0.0-bug-fixed

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
         .center{
-            text-align: center;/*3-5 2nd Jugnoo*/
+            text-align: center;
         }
         .header-container{
             background: #000;
