7613dc6f3a0e95a02cbe0906c72b9238696ebfc7
Swetank Subham <swetanksubham.r@gmail.com>
2018-08-22 18:26:09 +0530

    1.0.0

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TOC | Swetank Subham</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <style>
         .center{
             text-align: center;
@@ -69,23 +70,38 @@
             color: #fff;
             text-align: left;
         }
+        .header{
+            background: linear-gradient(to bottom, rgba(0, 116, 249, 0.8), rgba(37, 63, 197, 1));
+            color: #fff;
+            padding: 5px;
+            font-size: 20px;
+            font-weight: bold;
+            text-align: center;
+        }
+        .head-btn-left{
+            border-radius: 0 0 0 6px;
+        }
+        .head-btn-right{
+            border-radius: 0 0 6px 0;
+        }
     </style>
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12 center pad align-items-start header-container">
-                <div id="header"><h1>Table of Contents</h1></header></div>
+                <div id="header"><h1>Workspace</h1></header></div>
             </div>
         </div>
         <div class="row" style="height: 12px;"></div>
         <div class="row">
-            <div class="offset-sm-2 card bg-faded card-block col-8 content-container">
+            <div class="card bg-faded card-block col-8 content-container">
                 <?php
                     if ($handle = opendir('..')) {
                         $counter = 1;
                         $path2proj = '.';
-                        echo "<p style='padding-bottom: 2px;' class='project-name'><span class='col-1 btn btn-blue project-counter-btn'>S.No.</span><span class='col-6 project-name-btn btn btn-black'>Project Name</span><span class='col-5 btn btn-green project-size-btn' style='color: #fff;'>Last Modified Date</span><br><p>";
+                        echo "<div class='header'>Table of Contents</div>";
+                        echo "<p style='padding-bottom: 2px;' class='project-name'><span class='col-1 btn btn-blue project-counter-btn head-btn-left'>S.No.</span><span class='col-6 project-name-btn btn btn-black'>Project Name</span><span class='col-5 btn btn-green project-size-btn head-btn-right' style='color: #fff;'>Last Modified Date</span><br><p>";
                         while (false !== ($entry = readdir($handle))) {
                             if ($entry != "." && $entry != "..") {
                                 if($entry == "Table of Content"){
@@ -100,6 +116,20 @@
                         closedir($handle);
                     }
                 ?>
+            </div>
+            <div class="col-4 card bg-faded card-block content-container">
+                <div class='header'>To-Do-List</div>
+                <div class="form">
+                    <form>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Create To-DO" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="button"><i class="fas fa-plus"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <p style="text-align: center; margin-top: 15px;">*Under development</p>
             </div>
         </div>
     </div>
